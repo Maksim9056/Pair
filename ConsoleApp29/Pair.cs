0925184f41574502f8804d66c72582c77f82aa7e
Maksim <bobreczovm@inbox.ru>
2023-10-26 10:42:41 +0300

    Правки

--- ConsoleApp29/Pair.cs
+++ ConsoleApp29/Pair.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp29
 {
+    /// <summary>
+    /// Pair
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="U"></typeparam>
     public class Pair<T, U>
     {
 
         public T Value { get; set; }
         public U Value2 { get; set; }
 
+
+        /// <summary>
+        /// Конструктор класса  Pair 2 параметр value и value2
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="value2"></param>
         public Pair(T value, U value2)
         {
             Value = value;
@@ -19,6 +30,10 @@ namespace ConsoleApp29
             Value2 = value2;
         }
 
+
+        /// <summary>
+        /// Меняет Значение переменых на оборот 
+        /// </summary>
         public void Swap()
         {
             object value1 = Value;
