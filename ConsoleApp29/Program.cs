fcd82da18205a2aa99666a984f5d8bf6d2773c45
Maksim <bobreczovm@inbox.ru>
2023-10-26 10:40:00 +0300

    Правки

--- ConsoleApp29/Program.cs
+++ ConsoleApp29/Program.cs
@@ -2,8 +2,21 @@
 {
     internal class Program
     {
+
+
+        /// <summary>
+        ///  Напишите обобщенный класс Pair<T, U>,
+        ///  который хранит пару значений типов T и U.
+        ///  Затем напишите ограничение для этого класса,
+        ///  чтобы он мог работать только с типами, 
+        ///  которые являются ссылочными типами (class). 
+        ///  Затем напишите метод void Swap(),
+        ///  который меняет местами значения пары
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+
             Pair<int, int> pair = new Pair<int, int>(1, 2);
 
             Console.WriteLine($"Параметры : {pair.Value}    {pair.Value2}"  );
