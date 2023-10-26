8de9915a8707bf6219c91ae3dd3d9e8a174f93e2
Maksim <bobreczovm@inbox.ru>
2023-10-26 10:31:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp29/Pair.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    public class Pair<T, U>
+    {
+
+        public T Value { get; set; }
+        public U Value2 { get; set; }
+
+        public Pair(T value, U value2)
+        {
+            Value = value;
+
+            Value2 = value2;
+        }
+
+        public void Swap()
+        {
+            object value1 = Value;
+
+
+            object value2 = Value2;
+
+            Value = (T?)value2;
+
+            Value2 = (U?)value1;
+
+        }
+    }
+}
---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair<int, int> pair = new Pair<int, int>(1, 2);
+
+            Console.WriteLine($"Параметры : {pair.Value}    {pair.Value2}"  );
+            pair.Swap();
+            Console.WriteLine($"Параметры : {pair.Value}    {pair.Value2}");
+
+            Console.ReadLine();
+        }
+    }
+}
